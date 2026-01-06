b54ba1526c22cdbcdb7ff0c0ed6954e4c3d4ce69
sidharthify <wednisegit@gmail.com>
2026-01-06 23:05:26 +0530

    serialExperimentsNix: 2026-01-06 23:05:26

--- nixos/hardware/intel.nix
+++ nixos/hardware/intel.nix
@@ -1,11 +1,12 @@
 # intel.nix
 
-{ config, pkgs, ... }:  
- 
+{ config, pkgs, ... }:
+
 {
   boot.kernelModules = [ "snd_hda_intel" ];
- 
+
   boot.extraModprobeConfig = ''
-  options snd-hda-intel model=snd-hda-codec-realtek
+    # disable power saving to prevent state confusion with the windows driver
+    options snd_hda_intel power_save=0 power_save_controller=N
   '';
 }

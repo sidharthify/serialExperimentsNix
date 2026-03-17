6d49f38fa33a184a7471ec67a39787d272f15cbe
sidharthify <wednisegit@gmail.com>
2026-03-17 14:10:42 +0530

    serialExperimentsNix: 2026-03-17 14:10:42

--- nixos/system/networking.nix
+++ nixos/system/networking.nix
@@ -1,23 +1,31 @@
-{ config, pkgs, ... }:  
+{ config, pkgs, ... }:
 
 {
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-  networking.interfaces.wlp3s0.useDHCP = false;
-  networking.interfaces."wlp3s0".ipv4.addresses = [{
-  address = "192.168.1.184";
-  prefixLength = 24;
-  }];
-  networking.defaultGateway = "192.168.1.1";
-  networking.resolvconf.enable = false;
-  networking.nameservers = [ "192.168.1.1" ];
-  networking.interfaces."wlp3s0".mtu = 1500;
-  networking.firewall.allowedTCPPorts = [ 25565 24800 5520 59100 59200 3478 443];
+
+  # Use DHCP and set MTU to 1372 for Jio 5G compatibility
+  networking.interfaces.wlp3s0 = {
+    useDHCP = true;
+    mtu = 1372;
+  };
+
+  # Prevent NetworkManager from resetting MTU and disable WiFi power save (1=keep, 2=disable, 3=enable)
+  networking.networkmanager.wifi.powersave = false;
+
+  networking.firewall.allowedTCPPorts = [ 25565 24800 5520 59100 59200 3478 443 ];
   networking.firewall.allowedUDPPorts = [ 5520 ];
   networking.enableIPv6 = false;
 
   boot.kernel.sysctl = {
+    "net.ipv4.tcp_congestion_control" = "bbr";
+    "net.core.default_qdisc" = "fq";
+    "net.ipv4.tcp_slow_start_after_idle" = 0;
+    "net.ipv4.tcp_mtu_probing" = 1;
     "net.ipv6.conf.all.mldv2_unsolicited_report_interval" = 0;
     "net.ipv6.conf.default.mldv2_unsolicited_report_interval" = 0;
-    };
+  };
+
+  # Load required modules for BBR
+  boot.kernelModules = [ "tcp_bbr" ];
 }

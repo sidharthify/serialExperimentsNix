64071ebd904d25378bd1d139f120d6cadd9b5ac1
sidharthify <wednisegit@gmail.com>
2026-03-18 01:56:05 +0530

    serialExperimentsNix: 2026-03-18 01:56:05

--- nixos/system/kernel.nix
+++ nixos/system/kernel.nix
@@ -1,6 +1,6 @@
  { config, pkgs, ... }:  
 
 { 
-  boot.kernelPackages = pkgs.linuxPackages; # switch to stable until nvidia updates their drivers
+  boot.kernelPackages = pkgs.linuxPackages_6_19;
   boot.kernelParams = [ "nvidia-modeset.hdmi_deepcolor=0" "nvidia.NVreg_EnableGpuFirmware=0" "nvidia_drm.modeset=1"];
 }

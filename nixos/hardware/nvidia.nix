eb3b2fc48e36e46e52258d5d6e3b692ea905580b
sidharthify <wednisegit@gmail.com>
2026-03-24 22:07:46 +0530

    serialExperimentsNix: 2026-03-24 22:07:46

--- nixos/hardware/nvidia.nix
+++ nixos/hardware/nvidia.nix
@@ -9,7 +9,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   boot.extraModprobeConfig = ''

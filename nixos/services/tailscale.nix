6d08ef7713c52092b297d578fa6c724e6de7e556
sidharthify <wednisegit@gmail.com>
2026-03-30 20:22:50 +0530

    serialExperimentsNix: 2026-03-30 20:22:50

--- nixos/services/tailscale.nix
+++ nixos/services/tailscale.nix
@@ -1,7 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  services.tailscale.enable = true;
+  services.tailscale = {
+    enable = true;
+    # "server" enables IP forwarding (ipv4 + ipv6) so this machine can act
+    # as a subnet router. After nixos-rebuild switch, run once:
+    #   sudo tailscale up --advertise-routes=<YOUR_LAN_CIDR>
+    #   e.g.  sudo tailscale up --advertise-routes=192.168.1.0/24
+    # Then approve the routes in https://login.tailscale.com/admin/machines
+    useRoutingFeatures = "server";
+  };
+
   networking.nftables.enable = true;
   networking.firewall = {
     enable = true;
@@ -9,10 +18,10 @@
     allowedUDPPorts = [ config.services.tailscale.port ];
   };
 
-  systemd.services.tailscaled.serviceConfig.Environment = [ 
-    "TS_DEBUG_FIREWALL_MODE=nftables" 
+  systemd.services.tailscaled.serviceConfig.Environment = [
+    "TS_DEBUG_FIREWALL_MODE=nftables"
   ];
 
-  systemd.network.wait-online.enable = false; 
+  systemd.network.wait-online.enable = false;
   boot.initrd.systemd.network.wait-online.enable = false;
 }

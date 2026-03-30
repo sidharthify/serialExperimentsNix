2dd40314e0dab3dcfd1c7f04677f8e14c3850574
sidharthify <wednisegit@gmail.com>
2026-03-30 20:44:39 +0530

    serialExperimentsNix: 2026-03-30 20:44:39

--- nixos/services/tailscale.nix
+++ nixos/services/tailscale.nix
@@ -12,11 +12,11 @@
   };
 
   networking.nftables.enable = true;
-  networking.firewall = {
-    enable = true;
-    trustedInterfaces = [ "tailscale0" ];
-    allowedUDPPorts = [ config.services.tailscale.port ];
-  };
+#  networking.firewall = {
+ #   enable = true;
+  #  trustedInterfaces = [ "tailscale0" ];
+   # allowedUDPPorts = [ config.services.tailscale.port ];
+  #};
 
   systemd.services.tailscaled.serviceConfig.Environment = [
     "TS_DEBUG_FIREWALL_MODE=nftables"

82fb68ba6cc40f197b79f64562f5017f492f9450
sidharthify <wednisegit@gmail.com>
2026-04-28 23:16:40 +0530

    serialExperimentsNix: 2026-04-28 23:16:40

--- nixos/hardware/nvidia.nix
+++ nixos/hardware/nvidia.nix
@@ -9,14 +9,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "595.58.03";
-      sha256_64bit = "sha256-jA1Plnt5MsSrVxQnKu6BAzkrCnAskq+lVRdtNiBYKfk=";
-      sha256_aarch64 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-      openSha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-      settingsSha256 = "sha256-2vLF5Evl2D6tRQJo0uUyY3tpWqjvJQ0/Rpxan3NOD3c=";
-      persistencedSha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-    };
+     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   boot.extraModprobeConfig = ''

d332fcdeac7578e66258b260819ba3179065f2f7
sidharthify <wednisegit@gmail.com>
2026-05-09 05:20:58 +0530

    serialExperimentsNix: 2026-05-09 05:20:58

--- nixos/system/kernel.nix
+++ nixos/system/kernel.nix
@@ -1,18 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  boot.kernelPackages = let
-    linux_7_0_1 = pkgs.linux_latest.override {
-      argsOverride = rec {
-        version = "7.0.1";
-        modDirVersion = version;
-        src = pkgs.fetchurl {
-          url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
-          sha256 = "1gw7v1j0pp2w6fm5y1n0krhnfvgab2jkrvcvwl8hx614dnikbjdj"; 
-        };
-      };
-    };
-  in pkgs.linuxPackagesFor linux_7_0_1;
+  boot.kernelPackages = pkgs.linuxPackages_testing;
 
   boot.kernelParams = [ 
     "nvidia-modeset.hdmi_deepcolor=0" 

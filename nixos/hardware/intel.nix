a1fa4875ce248e39d3d70ea4515530416d632a0d
sidharthify <wednisegit@gmail.com>
2026-05-23 11:22:59 +0530

    serialExperimentsNix: 2026-05-23 11:22:59

--- nixos/hardware/intel.nix
+++ nixos/hardware/intel.nix
@@ -8,6 +8,6 @@
   boot.extraModprobeConfig = ''
     # disable power saving to prevent state confusion with the windows driver
     options snd_hda_intel power_save=0 power_save_controller=N
-    options usb_storage quirks=0bda:9210:u
+    options usb_storage quirks=0bda:9210:b
   '';
 }

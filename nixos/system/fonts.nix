ea1c9b5991a400a9e3c1fd1581fe5cc81bff8b4a
sidharthify <wednisegit@gmail.com>
2026-05-25 12:14:13 +0530

    serialExperimentsNix: 2026-05-25 12:14:13

--- nixos/system/fonts.nix
+++ nixos/system/fonts.nix
@@ -1,12 +1,36 @@
- { config, pkgs, ... }:  
+{ config, pkgs, ... }:
 
 {
-  # Fonts
-  fonts.packages = with pkgs; [
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-color-emoji
-    inter
-    jetbrains-mono
-  ];
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      inter
+      jetbrains-mono
+      hack-font
+    ];
+
+    fontconfig = {
+      enable = true;
+      antialias = true;
+      hinting = {
+        enable = true;
+        autohint = false; 
+        style = "slight";
+      };
+
+      subpixel = {
+        rgba = "rgb";
+        lcdfilter = "default";
+      };
+
+      defaultFonts = {
+        sansSerif = [ "Inter" "Noto Sans CJK SC" "Noto Color Emoji" ];
+        serif = [ "Noto Serif CJK SC" "Noto Color Emoji" ];
+        monospace = [ "JetBrains Mono" "Noto Color Emoji" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
 }
